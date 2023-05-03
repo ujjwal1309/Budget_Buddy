ac38813d51d757cfad35ba566c1c7bb31b4dbc22
paulzpranay97 <paulpranay1997@gmail.com>
2023-05-03 23:09:42 +0530

    done

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const { connection } = require("./config/db");
-require("dotenv").config();
-
+const express = require('express');
+const mongoose = require('mongoose');
+const {DataRouter} = require('./routes/data.route');
+const {connection} = require("./config/db")
+require('dotenv').config()
 const app = express();
+
+
 app.use(express.json());
-app.use(cors());
-app.set("views", __dirname + "/public");
-app.set("view engine", "ejs");
-app.use(express.static("./public"));
+app.use('/budgetbuddy', DataRouter);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
-});
-
-port = process.env.PORT || 4000;
-
-app.listen(port, async () => {
-  try {
-    console.log(`server is running at http://localhost:${port}`);
-    await connection;
-    console.log("DB is connected");
-  } catch (error) {
-    console.log("DB isn't connected");
-    console.log(error)
+app.listen(process.env.PORT,async(res,err)=>{
+  try{
+      await connection
+      
+      console.log(`connected to db ${process.env.PORT}`);
+  }catch(err){
+      console.log(err);
   }
-});
+})
