fc33aa39327e932ea842083f759d6d37e104efd2
Chandan Kumar Behera <118505620+chandankumar1425@users.noreply.github.com>
2023-05-04 16:12:17 +0530

    fw24_463 day-02

--- models/user.model.js
+++ models/user.model.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
   {
-    first_name: {type:String,required:true},
-    last_name:{type:String,required:true},
-    email: {type:String,required:true},
-    mobile_number:{type:Number,required:true},
-    password: {type:String,required:true},
+    first_name: {type:String},
+    last_name:{type:String},
+    email: {type:String},
+    mobile_number:{type:Number},
+    password: {type:String},
   },
   {
     versionKey: false,
---
+++ public/script/index.js
@@ -0,0 +1,3 @@
+document.getElementById("myVideo").loop = true;
+document.getElementById("myVideo2").loop = true;
+document.getElementById("myVideo3").loop = true;
